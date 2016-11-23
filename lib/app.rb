289d4daf9613bb2f332fcce7b7e815affe3d0a37
Bryony Watson <bryonywatson1@googlemail.com>
2016-11-23 14:35:30 +0000

    its working hoorraayyy step 19

--- lib/app.rb
+++ lib/app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 set :sessions, true
+enable :sessions
 
 
   get '/' do
@@ -13,9 +14,15 @@ set :sessions, true
 
 
   post '/names' do
-    @name1 = params[:name1]
-    @name2 = params[:name2]
-    erb :play
+    session[:name1] = params[:name1]
+    session[:name2] = params[:name2]
+     redirect '/play'
+  end
+
+  get '/play' do
+    @name1 = session[:name1]
+    @name2 = session[:name2]
+     erb :play
   end
 
   run! if app_file == $0
