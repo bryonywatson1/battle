905249e8b07e21697702f4866f227764bf535fd9
pflarcology <rhrg500@gmail.com>
2016-11-23 14:59:32 +0000

    adds hitpoints for player 2

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,10 +2,20 @@ require 'spec_helper'
 
 RSpec.feature "user names", :type => :feature do
   scenario "user submits names" do
-    visit('http://localhost:4567/')
+    visit('/')
     fill_in "name1", :with => "a"
-    fill_in "name2", :with => "b"
+    fill_in "name2", :with => "c"
     click_button('Submit')
-    expect(page).to have_content('a vs. b')
+    expect(page).to have_content('a vs. c')
+  end
+end
+
+RSpec.feature "hit points", :type => :feature do
+  scenario "view player 2's hit points" do
+    visit('/')
+    fill_in "name1", :with => "a"
+    fill_in "name2", :with => "c"
+    click_button('Submit')
+    expect(page).to have_content(': 10HP')
   end
 end
