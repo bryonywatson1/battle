eab4fead6dab21fa5db5cdc35c18471d47bbea5f
DarkChocLatte <darkchoclatte@gmail.com>
2016-11-25 09:49:02 +0000

    changes

--- spec/features/player_spec.rb
+++ spec/features/player_spec.rb
@@ -2,10 +2,12 @@ require 'player.rb'
 
 describe Player do
 
+  let(:user_name) {double(:user_name)}
+  subject(:player) {described_class.new(user_name)}
+
   context "when players method name is called" do
 
-    let(:user_name) {double(:user_name)}
-    subject(:player) {described_class.new(user_name)}
+
 
     it "should return the name" do
       expect(player.name).to eq(user_name)
@@ -15,6 +17,7 @@ describe Player do
   context "When players method hp is called" do
 
     it "should return 60 a default of 60" do
+
       expect(player.hp).to eq 60
     end
   end
